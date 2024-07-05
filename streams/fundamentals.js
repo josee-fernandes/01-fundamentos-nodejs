8ac15499f5b9309f166904fb08c028cea6273f7d
José Vitor <jose-vitor-7@hotmail.com>
2024-07-05 07:07:04 -0300

    Streams no Node.js - Criando stream de leitura

--- streams/fundamentals.js
+++ streams/fundamentals.js
@@ -1,13 +1,27 @@
-// Netflix & Spotify
+// Streams -> 
 
-// Importação de clientes via CSV (Excel)
-// 1gb - 1.000.000 linhas
-// POST /upload import.csv
+// process.stdin
+//   .pipe(process.stdout)
 
-// 10mb/s - 820s
+import { Readable } from 'node:stream'
 
-// 100s -> Inserções no banco de dados
+class OneToHundredStream extends Readable {
+  index = 1
 
-// 10mb/s -> 10.000
+  _read() {
+    const i = this.index++
 
-// Readable Streams / Writable Streams
+    setTimeout(() => {
+      if (i > 100) {
+        this.push(null)
+      } else {
+        const buf = Buffer.from(String(i))
+  
+        this.push(buf)
+      }
+    }, 1000)
+  }
+}
+
+new OneToHundredStream()
+  .pipe(process.stdout)
