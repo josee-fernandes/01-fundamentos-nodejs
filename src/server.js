845506c09dde66975cc4ef4d3e89e08f8d04377e
José Vitor <jose-vitor-7@hotmail.com>
2024-07-06 04:15:56 -0300

    Rotas da aplicação - Separando rotas da aplicação

--- src/server.js
+++ src/server.js
@@ -1,7 +1,6 @@
 import http from 'node:http'
-import { randomUUID } from 'node:crypto'
 import { json } from './middlewares/json.js'
-import { Database } from './database.js'
+import { routes } from './routes.js'
 
 // - Criar usuários
 // - Listagem de usuários
@@ -32,31 +31,16 @@ import { Database } from './database.js'
 // 4xx Erro do cliente
 // 5xx Erro do servidor
 
-const database = new Database()
 
 const server = http.createServer(async (request, response) => {
   const { method, url } = request
 
   await json(request, response)
 
-  if (method === 'GET' && url === '/users') {
-    const users = database.select('users')
+  const route = routes.find(route => route.method === method && route.path === url)
 
-    return response.end(JSON.stringify(users))
-  }
-
-  if (method === 'POST' && url === '/users') {
-    const { name, email } = request.body
-
-    const user = {
-      id: randomUUID(),
-      name,
-      email,
-    }
-
-    database.insert('users', user)
-
-    return response.writeHead(201).end()
+  if (route) {
+    return route.handler(request, response)
   }
 
   return response.writeHead(404).end()
